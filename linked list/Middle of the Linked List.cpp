93cf64bb128fb99dc64b227263b3fddfa26a846a
agatya mishra <agatyamishra@gmail.com>
2023-06-07 13:41:58 +0530

    Middle of the Linked List

---
+++ linked list/Middle of the Linked List.cpp
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode *start,*tail;
+        start=tail=head;
+        while(start && start->next){
+           start= start->next->next;
+           tail= tail->next;
+        }
+        return tail;
+    }
+};
