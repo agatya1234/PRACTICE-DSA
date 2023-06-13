ccfd0a0f3d0c317dd6dba86ebf8812fedb869462
agatya mishra <agatyamishra@gmail.com>
2023-06-13 23:16:28 +0530

    Best Time to Buy and Sell Stoc

---
+++ Arrya/Best Time to Buy and Sell Stock.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int min_profit =INT_MAX;
+        int max_profit = 0;
+        for(int i=0;i<prices.size();i++){
+         min_profit = min(min_profit, prices[i]);
+        max_profit = max(max_profit, prices[i] - min_profit);
+    }
+    
+    return max_profit;  
+    }
+};
