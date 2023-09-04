9520c681793f240c4177c59d9ad87529136ca2b6
agatya mishra <agatyamishra@gmail.com>
2023-09-05 00:04:14 +0530

    intersction of two linkedlist

--- linked list/intersection of two linkedlist.cpp
+++ linked list/intersection of two linkedlist.cpp
@@ -8,17 +8,41 @@
  */
 class Solution {
 public:
+int length(ListNode *head){
+        int len = 0;
+        while(head){
+            len++;
+            head = head->next;
+        }
+        return len;
+    }
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-       
-        unordered_map<ListNode*,int>mpp;
-    for (auto p = headA ; p!=NULL ; p = p->next){
-        mpp[p]=p->val;
-    }
-    
-    for (auto p = headB ; p!=NULL ; p = p->next){
-        if (mpp.find(p)!=mpp.end()) return p;
-    }
-    return NULL;
+       if(!headA || !headB) return NULL;
+
+        //step1
+        int lenA = length(headA), lenB = length(headB);
+
+        //step2
+        if(lenA>lenB){
+            while(lenA>lenB){
+                headA = headA->next;
+                lenA--;
+            }
+        }
+        else if(lenA<lenB){
+            while(lenA<lenB){
+                headB = headB->next;
+                lenB--;
+            }
+        }
+        
+        //step 3
+        while(headA && headB){
+            if(headA==headB) return headA;
+            headA = headA->next;
+            headB = headB->next;
+        }
+        return NULL;
     
     }
 };
