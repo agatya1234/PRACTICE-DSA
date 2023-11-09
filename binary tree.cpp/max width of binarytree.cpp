d5ba3bdd2fe4efa9ea22efd6527e6afcc2974da1
agatya mishra <agatyamishra@gmail.com>
2023-11-09 12:30:15 +0530

    max width of binarytree

--- binary tree.cpp/max width of binarytree.cpp
+++ binary tree.cpp/max width of binarytree.cpp
@@ -38,3 +38,32 @@ public:
         return Max_width;
     }
 };
+class Solution {
+public:
+    int widthOfBinaryTree(TreeNode* root) {
+        if (root == NULL) return 0;
+        queue<pair<TreeNode*,int>>q;//storing node and its index
+        q.push({root,0});//at staring we are inserting root and its index.
+        long ans=0;
+        while(!q.empty()){
+            int n=q.size();
+            int start=q.front().second;//find start or leftmost  index of present level
+            int end=q.back().second;//find end or rightmost index of present level
+            ans = max<long>(ans , end-start+1);//find max btw ans and starting and ending
+           for(int i=0;i<n;i++){
+               pair<TreeNode*,int>p=q.front();
+               TreeNode* node =p.first;
+               long idx=p.second;//idx is parent node
+               q.pop();
+               if(node->left)q.push({node->left,2*idx+1});
+               if(node->right)q.push({node->right,2*idx+2});
+           }
+        }
+        return ans;
+
+
+        
+        
+    }
+};
+
