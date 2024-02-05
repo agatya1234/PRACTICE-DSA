88b2c9bb3f70f62f4a883293b0d684f6ef0e6c8d
agatya mishra <agatyamishra@gmail.com>
2024-02-05 23:07:11 +0530

    valid triangle number

---
+++ binary search/valid triangle number.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int triangleNumber(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int ans=0;
+        int n=nums.size();
+        for(int i=n-1;i>=2;i--){
+            int low=0;
+            int high=i-1;
+            while(low<=high){
+                if(nums[low]+nums[high]>nums[i]){
+                    ans=ans+(high-low);
+                    high--;
+                }
+                else{
+                    low++;
+                }
+            }
+        }
+        return ans;
+    }
+};
