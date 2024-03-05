244c588ad81b13ee5198d4361fd40ad67d1ec989
agatya mishra <agatyamishra@gmail.com>
2024-03-05 10:13:39 +0530

    min length of string after deleting similar ends

---
+++ march badge/min length of string after deleting similar ends.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minimumLength(string s) {
+       int n=s.size();
+       int left=0;
+       int right=n-1;
+      while(left<right && s[left]==s[right]){
+          char ch=s[left];
+       while(left<=right && s[left]==ch){
+           left++;
+       }
+       while(left<=right && s[right]==ch){
+           right--;
+       }
+      }
+      return right-left+1;
+    }
+};
