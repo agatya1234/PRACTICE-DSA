01da53f1c7605b13be1da60813e15890463643fa
agatya mishra <agatyamishra@gmail.com>
2024-04-02 22:21:51 +0530

    isomorphic string

---
+++ april badge/isomorphic string.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+       int n=s.size();
+       char seen[128]={};
+       for(int i=0;i<n;i++){
+        char c=s[i];
+        if(!seen[c]){
+            for(char s:seen)if(s==t[i])return false;
+            seen[c]=t[i];
+        }
+        else if(seen[c]!=t[i])return false;
+       }
+        return true;
+    }
+};
